d6d6656e8c3248a215e18ab12fe96506c6c3b645
Apollo122383 <apollo.morais@escola.pr.gov.br>
2026-03-16 12:10:49 -0300

    save

--- aulas/13032026.js
+++ aulas/13032026.js
@@ -1,14 +1,16 @@
-let lucro = 30;
-let lucro2 = 10;
-
-function soma(a,b){
-    return a + b
+function calculadora(ari,a,b){
+    switch(ari){
+        case '+':
+            resultado = a + b;
+            break;
+        case '-':
+            resultado = a - b;
+            break;
+        case '*':
+            resultado = a * b;
+            break;
+        case '/':
+            resultado = a / b;
+            break;
+    }
 }
-soma(lucro, lucro2)
-
-function resultadoDivisaoSocio(lucro, lucro2){
-    return soma(lucro, lucro2)/2
-}
-
-
-    
