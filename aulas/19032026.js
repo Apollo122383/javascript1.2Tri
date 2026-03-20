19ac8dbd2ef538c776cce144a2f77707554de64a
Apollo122383 <apollo.morais@escola.pr.gov.br>
2026-03-20 07:53:33 -0300

    update

--- aulas/19032026.js
+++ aulas/19032026.js
@@ -8,3 +8,22 @@ let decimal = 20.30
 
 //STring
 let nome = "Meu nome é antoio"
+let nome1 = 'MEU NOME É ANTOIO'
+
+console.log(cidade===input); //False
+
+const inputMinusculo = nome.toLowerCase();
+
+console.log(nome1 === inputMinusculo); //True
+
+const senha = 'MinhaSenha123'
+console.log(senha.length) //13 caracteres
+
+//Booleanos
+
+let nv = true;
+let nz = false;
+
+if(nv!==nz){
+    console.log('nv é diferente, pois ele é ${nv}')
+}
